6a0ff41aeae692ade1257f91603be8e05aaa445a
Asser Osama <163314009+Asserosa@users.noreply.github.com>
2024-10-11 15:30:59 -0700

    Add files via upload

---
+++ programming/script.js
@@ -0,0 +1,17 @@
+
+function change(select, divId) {
+    var selectedValue = select.value;
+    document.getElementById(divId).style.background = selectedValue;
+}
+
+function change(select, divId) {
+    var selectedValue = select.value;
+    document.getElementById(divId).style.background = selectedValue;
+}
+
+function change(select, divId) {
+    var selectedValue = select.value;
+    document.getElementById(divId).style.background = selectedValue;
+}
+
+
